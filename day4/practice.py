d508ff43eac43c27e1f8b0b7b19a48cb50a7adb4
Alex Moñiz <amo@GALELAP154.local>
2023-04-04 15:56:15 +0200

    04042023 15.55h

--- day4/practice.py
+++ day4/practice.py
@@ -1,1 +1,13 @@
-# Hello World!
+import random
+
+random_int = random.randint(100, 250)
+print(random_int)
+
+# random de 0.0 a 5.0
+random_float = random.random() * 5 
+print(random_float) 
+
+fruits = ["Apple", "Pear"]
+fruits.append("Banana") # Añadir un ítem al final de la lista
+fruits.extend(["Banana", "Orange"]) # Añadir varios ítems al final de la lista
+print(fruits)
