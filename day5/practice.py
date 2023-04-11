8a4d034618cb8893b991ddb264301eeba43507f2
Alex Moñiz <amo@GALELAP154.local>
2023-04-11 17:41:10 +0200

    11/04/2023 17:41

---
+++ day5/practice.py
@@ -0,0 +1,13 @@
+# Loops
+
+fruits = ["Apple", "Peach", "Pear"]
+years = [1, 200, 2020]
+
+for fruit in fruits:
+    print(fruit)
+    
+print(max(years)) # MAX() Averigua el máximo de una lista
+print(sum(years)) # SUM() Suma todos los valores de una lista
+
+for number in range(1, 10, 2): # range(principio, final, saltos)
+    print(number)
